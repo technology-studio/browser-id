7b64eb1f7b9d0a6c989c88f4cf2ca728271e983b
Erik Slovak <erik.slovak@technologystudio.sk>
2021-08-10 19:24:13 +0200

    add constant for local storage item key

--- packages/@txo/browser-id/src/index.ts
+++ packages/@txo/browser-id/src/index.ts
@@ -6,10 +6,12 @@ declare global {
   }
 }
 
-const existingId = window.localStorage.getItem('TXO_BROWSER_ID')
+const TXO_BROWSER_ID = 'txo-browser-id'
+
+const existingId = window.localStorage.getItem(TXO_BROWSER_ID)
 if (!existingId) {
   const id: string = v4()
-  window.localStorage.setItem('TXO_BROWSER_ID', id)
+  window.localStorage.setItem(TXO_BROWSER_ID, id)
   window.TXO_BROWSER_ID = id
 } else {
   window.TXO_BROWSER_ID = existingId
